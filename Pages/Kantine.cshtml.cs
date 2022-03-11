183942e5282f05a9bb1e98efa92ed758fe257012
nicolaiskat <jegerrockstar@gmail.com>
2022-03-11 11:09:56 +0100

    dsa

---
+++ Pages/Kantine.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBAS_menu.Pages;
+
+public class KantineModel : PageModel
+{
+    private readonly ILogger<KantineModel> _logger;
+
+    public KantineModel(ILogger<KantineModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
